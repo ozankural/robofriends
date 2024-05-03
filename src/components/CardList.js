56c36fccdf347340efd795a53941d510fcb3c092
Ozan <oznkurl@gmail.com>
2024-05-03 17:56:26 +0300

    update

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,23 +2,20 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({robots}) => {
-    if (false) {
-        throw new Error('Nooooo!')
-    }
     return (
         <div>
-        {
-        robots.map((user, i) => {
-            return (
-                <Card 
-                    key={i} 
-                    id={robots[i].id} 
-                    name={robots[i].name} 
-                    email={robots[i].email}
-                />
-            );
-        })
-        }
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card 
+                            key={i} 
+                            id={robots[i].id} 
+                            name={robots[i].name} 
+                            email={robots[i].email}
+                        />
+                    );
+                })
+            }
         </div>
     );
 }
